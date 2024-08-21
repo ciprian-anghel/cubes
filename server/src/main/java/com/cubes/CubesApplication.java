97456261f83afff0e941b2197dcb67126f07e25c
ciprian-anghel <anghelciprian91@gmail.com>
2024-08-21 20:58:16 +0300

    server

---
+++ server/src/main/java/com/cubes/CubesApplication.java
@@ -0,0 +1,13 @@
+package com.cubes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubesApplication.class, args);
+	}
+
+}
