7fcbe57f5fdbe584baed7db8a25392d5880375b5
ciprian-anghel <anghelciprian91@gmail.com>
2024-10-18 15:59:14 +0300

    test changes

--- server/src/main/java/com/cubes/config/GoogleFirebaseConfig.java
+++ server/src/main/java/com/cubes/config/GoogleFirebaseConfig.java
@@ -1,14 +1,18 @@
 package com.cubes.config;
 
+import java.io.IOException;
+
 import javax.naming.ConfigurationException;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 
 @Configuration
 public class GoogleFirebaseConfig {
@@ -19,17 +23,23 @@ public class GoogleFirebaseConfig {
 	}
 	
 	@Bean
-	FirebaseApp intializeFirebase() {
-		FirebaseApp app = FirebaseApp.initializeApp();
-		return app;
+	FirebaseApp intializeFirebase() throws IOException {
+		FirebaseOptions options = FirebaseOptions.builder()
+				.setCredentials(GoogleCredentials.getApplicationDefault())
+				.setStorageBucket(getEnvironment().getBucketName())
+				.build();
+		
+		return FirebaseApp.initializeApp(options);
 	}
 	
 	@Bean
 	Bucket getBucket() throws ConfigurationException {
-		Storage storage = StorageOptions.newBuilder()
-				.setProjectId(getEnvironment().getProjectId())
-				.build()
-				.getService();
+//		Storage storage = StorageOptions.newBuilder()
+//				.setProjectId(getEnvironment().getProjectId())
+//				.build()
+//				.getService();
+		
+		Storage storage = StorageOptions.getDefaultInstance().getService();
 		
 		Bucket bucket = storage.get(getEnvironment().getBucketName());
 		if (bucket != null) {
